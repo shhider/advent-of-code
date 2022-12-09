7e4a0a2bb8dbe22ed00b15cf692b27b767c1eafa
haohong.shh <haohong.shh@alibaba-inc.com>
2022-12-09 16:43:53 +0800

    day1 part2 completed

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -7,12 +7,19 @@ fn main() {
   let ipt_file = fs::File::open(INPUT_FILE).expect("invalid input file");
   let ipt_reader = BufReader::new(ipt_file);
 
-  let mut result: u32 = 0;
+  let mut top3: Vec<u32> = vec![0, 0, 0];
+  let mut the_max: u32 = 0;
   let mut count: u32 = 0;
   for line in ipt_reader.lines() {
     if let Ok(ll) = line {
-      if ll == "" {
-        result = result.max(count);
+      if ll.is_empty() {
+        let num3 = top3.pop().expect("unexpected").max(count);
+        top3.push(num3);
+        top3.sort();
+        top3.reverse();
+
+        the_max = the_max.max(count);
+
         count = 0;
       } else {
         let item: u32 = ll.trim().parse::<u32>().expect("should be a number");
@@ -21,5 +28,8 @@ fn main() {
     }
   }
 
-  println!("max Calorie: {}", result);
+  let top3_total: u32 = top3.iter().sum();
+
+  println!("max Calorie: {}", the_max);
+  println!("total calorie of top3: {}", top3_total);
 }
